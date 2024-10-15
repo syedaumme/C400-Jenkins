79a3d4be9effddf9fe7d4a55d78ae0ac5bdce699
Syeda Umme Kulsum <75966861+syedaumme@users.noreply.github.com>
2024-10-15 20:48:31 +0530

    app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World!! this is kulsum!'
+    return 'Hello World!! this is kulsum!!!!!!!!!'
 
 # main driver function
 if __name__ == '__main__':
